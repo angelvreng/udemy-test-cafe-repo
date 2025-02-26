edf244a7855abb62b4c9ff6e214a9b7bf0f3d79c
Angel Vazquez Rivero <angelvazquez@Angels-MacBook-Pro.local>
2025-02-26 11:00:24 +0100

    second commit

--- tests/login.test.js
+++ tests/login.test.js
@@ -5,7 +5,7 @@ fixture `Login Test`
     .page `https://www.saucedemo.com`;
 
 // Login with invalid credentials
-test.timeouts({ pageLoadTimeout: 60000 })(
+test.timeouts({ pageLoadTimeout: 6000 })(
 	'User cannot login with invalid credentials',
 	async t => {
 		const usernameInput = Selector('#user-name')
@@ -22,5 +22,31 @@ test.timeouts({ pageLoadTimeout: 60000 })(
 			.contains(
 				'Epic sadface: Username and password do not match any user in this service'
 			)
+	}
+)
+
+// Login with valid credentials
+test.timeouts({ pageLoadTimeout: 6000 })(
+	'User can login with valid credentials',
+	async t => {
+		const usernameInput = Selector('#user-name')
+		const passwordInput = Selector('#password')
+		await t.typeText(usernameInput, 'standard_user', { paste: true })
+		await t.typeText(passwordInput, 'secret_sauce', { paste: true })
+
+		const submitButton = Selector('#login-button')
+		await t.click(submitButton)
+
+		const accountLogo = Selector('.app_logo')
+		await t.expect(accountLogo.exists).ok() // Account logo after should exist
+		await t.expect(submitButton.exists).notOk() // Submit button from login should not exist
+
+		const logOutButton = Selector('#logout_sidebar_link')
+		await t.expect(logOutButton.exists).ok() // Logout button should exist
+
+		const expandMenuButton = Selector('#react-burger-menu-btn')
+		await t.click(expandMenuButton)
+		await t.click(logOutButton)
+		await t.expect(submitButton.exists).ok() // Submit button should exist
 	}
 )
